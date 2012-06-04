881abc1c23d3608fc00290f801695886f8c0c708
hokorobi <hokorobi.hokorobi@gmail.com>
2012-06-04 21:53:52 +0900

    add py2exe setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+import py2exe
+
+setup(options = {"py2exe": {
+                            'bundle_files': 1,
+                            'optimize': 2,
+                            'compressed': True}},
+      windows = ['multipletimer.py'], zipfile=None)
