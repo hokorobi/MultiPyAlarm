2f97f08fc8e2a6c70eb38882c24ea4020cd2da93
hokorobi <hokorobi.hokorobi@gmail.com>
2012-06-07 15:40:02 +0900

    setup.py の書き方をちょっと変えてみた。

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from distutils.core import setup
 import py2exe
 
-setup(options = {"py2exe": {
+target = py2exe.build_exe.Target(script='MultiPyAlarm.py')
+setup(options={"py2exe": {
                             'bundle_files': 1,
                             'optimize': 2,
-                            'compressed': True}},
-      windows = ['MultiPyAlarm.py'], zipfile=None)
+                            'compressed': True
+                            }
+                 },
+      windows=[target],
+      zipfile=None
+      )
