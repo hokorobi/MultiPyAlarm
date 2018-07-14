941d13f5a04ee9387e6d33b81144f009251aa651
hokorobi <hokorobi.hokorobi@gmail.com>
2018-07-15 01:18:57 +0900

    ログファイルをカレントディレクトリでなく実行するスクリプト, exe のパスに作成

--- logger.py
+++ logger.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from logging import getLogger, Formatter, FileHandler, INFO
+from pathlib import Path
+import sys
 
 
 class Logger(object):
     def __init__(self):
         self.logger = getLogger(__name__)
-        self.handler = FileHandler('MultiPyAlarm.log')
+        self.handler = FileHandler(Path(sys.argv[0]).resolve().parent / 'MultiPyAlarm.log')
         self.handler.setLevel(INFO)
         formatter = Formatter('%(asctime)s\t%(levelname)s\t%(message)s')
         self.handler.setFormatter(formatter)
